6ff373074c0f9269ab7a759c8d385b0360b0ae72
Hyungro Lee <hroe.lee@gmail.com>
2015-12-10 21:25:20 +0000

    collecting response time per request from nginx test.

---
+++ results-web.sh
@@ -0,0 +1,20 @@
+
+tool='docker rkt lxc'
+app='nginx'
+
+for k in $tool
+do
+	TMP2=""
+	for j in `echo 1 10 100 1000`
+	do
+		TMP=""
+		for i in `seq 1000 1000 10000`
+		do
+			#echo $k.$app.$i.$j.txt
+			RES=`grep 'Time per request' test-nginx/$k.$app.$i.$j.txt |grep 'across'|cut -d":" -f2|awk ' { print $1 } '`
+			TMP=$TMP`echo -n "$RES,"`
+		done
+		TMP2=$TMP2`echo "[$TMP],"`
+	done
+ 	echo "[$TMP2]" > $k.$app.result.csv
+done
